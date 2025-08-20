fb541d113c9c9d4e511dd907bbb93b82126fbfb7
Nikita Ulansky <nikulans@gmail.com>
2025-08-20 15:14:42 +0300

    брейкпоинт система

--- src/03-widgets/header/ui/HeaderWidget.vue
+++ src/03-widgets/header/ui/HeaderWidget.vue
@@ -38,29 +38,35 @@ const navList = ref([
 
 <template>
   <header class="header">
-    <router-link class="header__logo" :to="{name: 'HomePage'}">
-      <img src="@/06-shared/images/logo.png" alt="GPT Lawyer logo"/>
-    </router-link>
-    <nav class="header__nav">
-      <router-link class="header__nav-link"
-                   v-for="(item, index) in navList"
-                   :key="index"
-                   :to="{name: item.link}"
-                   :aria-label="item.ariaLabel">
-        <Icon :type="item.icon"/>
-        <p>{{ item.name }}</p>
+    <div class="header__content container">
+      <router-link class="header__logo" :to="{name: 'HomePage'}">
+        <img src="@/06-shared/images/logo.png" alt="GPT Lawyer лого"/>
       </router-link>
-    </nav>
+      <nav class="header__nav">
+        <router-link class="header__nav-link"
+                     v-for="(item, index) in navList"
+                     :key="index"
+                     :to="{name: item.link}"
+                     :aria-label="item.ariaLabel">
+          <Icon :type="item.icon"/>
+          <p>{{ item.name }}</p>
+        </router-link>
+      </nav>
+    </div>
   </header>
 </template>
 
 <style lang="scss">
 .header {
+  padding: 1rem 0;
   background: var(--card-color);
-  padding: 16px 32px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+
+  &__content {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 
   &__logo {
     display: flex;
@@ -68,7 +74,7 @@ const navList = ref([
     align-items: center;
 
     img {
-      width: 120px;
+      width: 5.5rem;
       height: auto;
     }
   }
@@ -77,14 +83,14 @@ const navList = ref([
     display: flex;
     flex-direction: row;
     align-items: center;
-    grid-gap: 20px;
+    grid-gap: 1.25rem;
   }
 
   &__nav-link {
     display: flex;
     flex-direction: row;
     align-items: center;
-    grid-gap: 5px;
+    grid-gap: 0.375rem;
     color: var(--text-color);
     transition: color 0.2s;
 
